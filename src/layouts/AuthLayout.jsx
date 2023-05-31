5a5918d06278126bb4bb0a57c4c3e35519d6ae93
Zhserg28 <serg280880@gmail.com>
2023-06-01 02:00:35 +0800

    react_ex_21

---
+++ src/layouts/AuthLayout.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+// Librares
+import { useSelector } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+// Components
+import Card from "../components/Card";
+// Pages
+import LoginPage from "../pages/LoginPage";
+import SignUpPage from "../pages/SigupPage";
+// Store
+import { isLoggedInSelector } from "../store/authSlice";
+
+const AuthLayout = () => {
+    // let { path } = useRouteMatch();
+    const isLoggedIn = useSelector(isLoggedInSelector());
+
+    // if (isLoggedIn) {
+    //     return <Redirect to='/' />;
+    // }
+
+    return (
+        <div className='flex grow flex-col justify-center items-center  dark:text-slate-200 '>
+            <Card>
+                <Routes>
+                    <Route path={"auth" + "/login"} element={<LoginPage/>} />
+                    <Route path={"auth" + "/signup"} element={<SignUpPage/>} />
+                    {/* <Redirect to={path + "/signup"} /> */}
+                </Routes>
+            </Card>
+        </div>
+    );
+};
+
+export default AuthLayout;
---
+++ src/layouts/PostsLayout.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from "react";
+// Librares
+import { useDispatch } from "react-redux";
+import { Routes, Route } from "react-router-dom";
+// Components
+import BackgroundGradiend from "../components/BackgroundGradient";
+import ScreenWidthWrapper from "../components/ScreenWidthWrapper";
+// Pages
+import PostPage from "../pages/Posts/PostPage";
+import PostsListPage from "../pages/Posts/PostsListPage";
+//Store
+import { getPosts } from "../store/postsSlice";
+
+const PostsLayout = ({ children }) => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const loadPostData = () => {
+            dispatch(getPosts());
+        };
+        loadPostData();
+    }, [dispatch]);
+
+    // let { path } = useRouteMatch();
+    return (
+        <>
+            <BackgroundGradiend />
+            <ScreenWidthWrapper>
+                <Routes>
+                    <Route path={"posts" + "/:postId"} element={<PostPage/>} />
+                    <Route path={"posts"} element={<PostsListPage/>} />
+                </Routes>
+                {children}
+            </ScreenWidthWrapper>
+        </>
+    );
+};
+
+export default PostsLayout;
