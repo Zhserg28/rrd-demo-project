f6cb09d18d34504aad692e2d2e80384f1ac9d7c2
Zhserg28 <serg280880@gmail.com>
2023-06-02 21:37:17 +0800

    ex_21_1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useRoutes } from "react-router-dom";
+import { useLocation, useRoutes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
 import NavBar from "./components/NavBar/NavBar";
@@ -13,7 +13,8 @@ import { isLoggedInSelector } from "./store/authSlice";
 
 function App() {
     const isLoggedIn = useSelector(isLoggedInSelector());
-    const elements = useRoutes(routes(isLoggedIn));
+    const location = useLocation();
+    const elements = useRoutes(routes(isLoggedIn,location));
     return (
         <div className='min-h-screen bg-slate-50 dark:bg-slate-900 transition-colors duration-150 flex flex-col'>
              <NavBar />
